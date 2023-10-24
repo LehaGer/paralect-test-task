ad4202fed000ad9d22b5003055cb32a64960f6f5
LehaGer <alexey.gerasimchuk@mail.ru>
2023-10-24 05:54:22 +0300

    refactor: stripe service initialisation changed to proper way

--- apps/api/src/services/stripe/stripe.service.ts
+++ apps/api/src/services/stripe/stripe.service.ts
@@ -1,11 +1,9 @@
 import config from 'config';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import Stripe from 'stripe';
 
-// const stripe = require('stripe')(config.STRIPE_SECRET_KEY);
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const stripe = new Stripe(config.STRIPE_SECRET_KEY);
+const stripe = new Stripe(config.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-08-16',
+  typescript: true,
+});
 
 export default stripe;
